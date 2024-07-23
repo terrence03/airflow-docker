701240ac2ff4d0d7c5f5407b38eb977a854c94d3
chienhua.hsu <chienhua.hsu@tri.org.tw>
2024-07-23 14:14:34 +0800

    feat: Add batch file for exporting oil data

---
+++ export_oil_data.bat
@@ -0,0 +1,4 @@
+CD  /d D:\Projects\airflow-docker
+CALL .\venv\Scripts\activate dev
+CALL python .\scripts\export_oil_data.py
+PAUSE
--- export_oil_data.ps1
+++
@@ -1,4 +0,0 @@
-cd D:\Projects\airflow-docker
-.\venv\Scripts\activate.ps1
-python .\scripts\export_oil_data.py
-exit
