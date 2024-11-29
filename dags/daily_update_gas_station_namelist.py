a7dc36c83052cec9d7e3ac1f1e004d57e7aa9893
Terrence Hsu <terrence0303@protonmail.com>
2024-11-29 14:41:08 +0800

    Add daily gas station namelist update DAG and data retrieval functionality

---
+++ dags/daily_update_gas_station_namelist.py
@@ -0,0 +1,39 @@
+import sys
+from pathlib import Path
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+sys.path.append("/opt/airflow")
+from plugins.gas_station.namelist import main
+
+db_path = Path("/opt/airflow/data/oilprice.db")
+
+default_args = {
+    "email": ["chienhua.hsu@tri.org.tw"],
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=30),
+}
+
+dag = DAG(
+    "daily_gas_station_namelist_update",
+    description="Update the gas station namelist daily",
+    schedule="30 8 * * *",
+    start_date=datetime(2024, 6, 15),
+    catchup=False,
+    tags=["daily", "oil"],
+    default_args=default_args,
+)
+
+
+t1 = PythonOperator(
+    task_id="daily_gas_station_namelist_update",
+    python_callable=main,
+    dag=dag,
+    retries=3,
+    retry_delay=timedelta(minutes=30),
+)
+
+t1
---
+++ plugins/gas_station/namelist.py
@@ -0,0 +1,80 @@
+import logging
+from pathlib import Path
+from datetime import datetime
+import requests
+import pandas as pd
+
+URL = "https://www2.moeaea.gov.tw/oil111/Dealer/GasStations/load"
+now = datetime.now()
+save_folder = Path("opt/ariflow/downloads/gas_station_namelist")
+log_file = save_folder / "update.log"
+
+
+class Log:
+    def __init__(self, log_file):
+        self.log_file = log_file
+        self.logger = self._setup_logger()
+
+    def _setup_logger(self):
+        """Set up the logger"""
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s - %(message)s")
+        file_handler = logging.FileHandler(self.log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        return logger
+
+    def write_update_info_to_logger(self, update_date: str, update_status: str):
+        """Test the logger"""
+        logger = self.logger
+        update_info = f"{update_date} - {update_status}"
+        logger.info(update_info)
+
+    def get_last_update_date(self):
+        """Get the last update date"""
+        with self.log_file.open() as _:
+            if not _.readlines():
+                return ""
+            _.seek(0)
+            last_line = _.readlines()[-1]
+            last_update_date = last_line.split(" - ")[1]
+            return str(last_update_date)
+
+
+def get_data() -> tuple[str, pd.DataFrame]:
+    """
+    Get the gas station data
+
+    Returns
+    -------
+    dict
+        The gas station data
+    """
+    response = requests.post(
+        URL,
+        headers={"User-Agent": "Mozilla/5.0"},
+        data={"city": "all"},
+    )
+    data = response.json()
+    data = data["data"]
+    update_time = data["UpdateTime"]
+    stations = data["stations"]
+    stations = pd.DataFrame(stations)
+    return update_time, stations
+
+
+def main():
+    log = Log(log_file)
+    update_time, stations = get_data()
+    last_update_date = log.get_last_update_date()
+    if update_time != last_update_date:
+        log.write_update_info_to_logger(update_time, "update")
+        stations.to_csv(
+            save_folder / f"{update_time.replace("/","-")}_全國加油站名單.csv",
+            index=False,
+        )
+        return True
+    else:
+        log.write_update_info_to_logger(update_time, "no update")
+    return False
