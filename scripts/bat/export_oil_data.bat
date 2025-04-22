310d13bf06d804a728dd79f8bde899738fedb09e
Terrence Hsu <terrence0303@protonmail.com>
2025-04-22 11:30:02 +0800

    Delete scripts directory

--- scripts/bat/export_oil_data.bat
+++
@@ -1,4 +0,0 @@
-cd  /d D:\Projects\airflow-docker
-call .\.venv\Scripts\activate dev
-call python .\scripts\python\export_oil_data.py
-pause
--- scripts/bat/sync_gas_station_namelist.bat
+++
@@ -1,3 +0,0 @@
-cd  /d D:\Projects\airflow-docker
-call .\.venv\Scripts\activate dev
-call python .\scripts\python\sync_gas_station_namelist.py
